87e5f5724aaa2f35fedfe4cef25b6e1733437a5e
Luke Wood <5woodl59@solent.ac.uk>
2021-10-01 12:19:45 +0100

    ascii + more

--- multiline_message1.py
+++ multiline_message1.py
@@ -7,3 +7,18 @@ print("...rebooting output motors")
 print("...rebooting hover engine")
 print()
 print("system online")
+
+# Display escape characters
+print("\n Displays a new line")
+print("\t Displays a tab space")
+print("\\ Displays a back slash")
+print("\" Displays a double quote")
+print("\' Displays a single quote")
+print()
+print(" I am Beep!")
+
+# Display a box
+print("##########")
+print("#        #")
+print("#        #")
+print("##########")

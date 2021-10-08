3ab1065902e314905c1965eff0cc91b653fcc556
Luke Wood <5woodl59@solent.ac.uk>
2021-10-08 12:21:54 +0100

    nested decisions

---
+++ multiple conditions.py
@@ -0,0 +1,5 @@
+type = input("What type of story?")
+if (type == "scary") or (type == "short"):
+    print("dark forest entered")
+elif (type == "safe") or (type == "long"):
+    print("safe route")
---
+++ nested desicions.py
@@ -0,0 +1,35 @@
+#cover = input("what type of cover do you have (soft/hard)")
+#if cover == "soft":
+#    condition = input("is it perfectly bound (yes/no)")
+#    if condition == "yes":
+#        print("Soft cover, perfect bound books are very popular")
+#    else:
+#        print("Soft covers with coils or stitches are great for short books")
+#elif cover == "hard":
+#    print("Books with hard covers can be more expensive!")
+#else:
+#    print("?")
+
+#where = input("where should I look? (bedroom/bathroom/lab)")
+#if where == "bedroom":
+#    where2 = input("where in the bedroom?")
+#    if where2 == "under the bed":
+#        print("shoes no battery")
+#    else:
+#        print("only a mess")
+#elif where == "bathroom":
+#    where2 = input("Where in the bathroom?")
+#    if where2 == "in the bathtub":
+#        print("rubber duck no battery")
+#    else:
+#        print("no battery")
+#elif where == "lab":
+#    where2 = input("where in the lab?")
+#    if where2 == "on the table":
+#        print("found it")
+#    else:
+#        print("not here")
+#else:
+#    print("not here")
+
+

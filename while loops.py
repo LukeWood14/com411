dfa9b5f70f482551dec7c1d9cf2e7d1fd133113c
Luke Wood <5woodl59@solent.ac.uk>
2021-10-08 12:51:27 +0100

    while loops started

---
+++ while loops.py
@@ -0,0 +1,20 @@
+#cables = int(input("how many cables to remove"))
+#removed = 0
+#while removed < cables:
+#    print("removed cable")
+#    removed = removed+1
+
+#livecables = int(input("how many live cables to avoid"))
+#avoided = 0
+#while avoided < livecables:
+#    print("Avoiding")
+#    avoided = avoided+1
+#    print(f"{avoided} live cables avoided")
+#print("all avoided")
+
+bars = int(input("how many bars need charging?"))
+x=0
+while x < bars:
+    x=x+1
+    print(f"charging ")
+

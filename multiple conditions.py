0c52b05ddaa3311265f01d87abd5a56b77f39188
Luke Wood <5woodl59@solent.ac.uk>
2021-10-08 12:38:04 +0100

    mult conditions

--- multiple conditions.py
+++ multiple conditions.py
@@ -1,5 +1,23 @@
-type = input("What type of story?")
-if (type == "scary") or (type == "short"):
-    print("dark forest entered")
-elif (type == "safe") or (type == "long"):
-    print("safe route")
+#type = input("What type of story?")
+#if (type == "scary") or (type == "short"):
+#    print("dark forest entered")
+#elif (type == "safe") or (type == "long"):
+#    print("safe route")
+#else:
+#    print("not sure")
+
+#big = input("is it big? yes/no")
+#heavy = input("is it heavy? yes/no")
+#if (heavy == "yes") and (big == "yes"):
+#    print("expensive")
+#else:
+#    print("cheap")
+
+#hear = input("what did I hear")
+#print(hear)
+#see = input("what did I see")
+#print(see)
+#if (hear == "grr") and (see == "two red eyes"):
+#    print("this is scary")
+#else:
+#    print("not scary")
